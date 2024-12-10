e94d916a868645082fda30b0eef95c29183cedf0
adriandbm <adrian2627@gmail.com>
2024-12-10 17:39:50 -0400

    Enlace home y vista en detalles listas

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, PublicationCreateView, PublicationDetailView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name="home"),
     path("publication/new/", PublicationCreateView.as_view(), name="publication_new") ,
     path("publication/<int:pk>/", PublicationDetailView.as_view(), name="publication_detail") ,
-    ]
+    ] 
