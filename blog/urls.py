17db5210e14f4f8bd1b9e3f310e791a192d33a36
adriandbm <adrian2627@gmail.com>
2024-12-10 18:27:11 -0400

    Update listo

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, PublicationCreateView, PublicationDetailView
+from .views import HomePageView, PublicationCreateView, PublicationDetailView, PublicationUpdateView 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path("publication/new/", PublicationCreateView.as_view(), name="publication_new") ,
     path("publication/<int:pk>/", PublicationDetailView.as_view(), name="publication_detail") ,
+    path("publication/<int:pk>/edit/", PublicationUpdateView.as_view(), name="publication_update")
     ] 
